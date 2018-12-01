8bc50d8a961eca984470abf3114b630238b5cf08
Kristiyan Todorov <45337186+krisScript@users.noreply.github.com>
2018-12-01 19:20:38 +0200

    Changing test description

--- src/app/getData/getData.test.js
+++ src/app/getData/getData.test.js
@@ -1,7 +1,7 @@
 import getData from './getData';
 jest.mock('./getData');
 describe('getData', () => {
-  it('should return', async () => {
+  it('should fetch data', async () => {
     const data = await getData();
     expect(data).toEqual([
       {
